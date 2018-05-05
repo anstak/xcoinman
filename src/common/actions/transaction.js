5a7965590616807c2bf378956d9ee3f9355b4fe1
anstak <anstak@gmail.com>
2018-05-05 17:45:35 +0300

    Fix Transaction bug

---
+++ src/common/actions/transaction.js
@@ -0,0 +1,42 @@
+import axios from '../../axios'
+import { REDIRECT_TO_TRANSACTION_FINISHED, GET_TRANSACTION_STATUS, START, SUCCESS, FAIL } from '../constants'
+import querystring from 'querystring';
+
+
+export function redirectToTransactionFinished(transactionData) {
+    return { 
+        type: REDIRECT_TO_TRANSACTION_FINISHED,
+        payload: transactionData
+    }
+}
+
+export function getTransactionStatus(id) {
+  return dispatch => {
+    dispatch({ type: GET_TRANSACTION_STATUS + START })
+    const params = querystring.stringify({ID: id});
+    return axios.post(
+        '/api/CryptoCurrencies/shapeshift-status',
+        params
+    )
+    .then(function (response) {
+      const {Data, Errors, Info, Type} = response.data
+        if (Errors.length === 0) {
+            dispatch({
+                type: GET_TRANSACTION_STATUS + SUCCESS,
+                payload: Data
+            })
+        } else {
+            dispatch({
+                type: GET_TRANSACTION_STATUS + FAIL,
+                errorMessage: Errors[0]
+            })
+        }
+    })
+    .catch(function (error) {
+      dispatch({
+        type: GET_TRANSACTION_STATUS + FAIL,
+        errorMessage: error
+      })
+    });
+  }
+}
